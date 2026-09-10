6c99f068cf8c6e7c07a7d74f984a6fc9ac826608
dakaraim <dmcclam@email.sc.edu>
2026-09-10 01:27:48 -0400

    added drinks and speech

--- csce242/04css/intro.js
+++ csce242/04css/intro.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+function showMessage() {
+    console.log{"I did it! I CLICKED THE BUTTON!"};
+}
+const binClick = document.getElementById("button-show-message");
+binClick - showMessage;
